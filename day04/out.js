02119510e370c4b55397e088c3fb64bdfc5f8df8
Kimjunmyung <79978429+Kimjunmyung@users.noreply.github.com>
2023-02-02 21:09:11 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,17 @@
+function 빨래하다() {  //함수. 여러 과정이 있는 처리를 묶어 이름을 부여하여 기능을 정의하는것. 
+    //1~6번까지 각각의 과정을 하나로 묶어 빨래하다 라는 이름을 부여하여 정의함.
+
+console.log("1. 먼지를 턴다.")
+ console.log("2. 물에 담군다.")
+ console.log("3. 비누칠을 한다.")
+ console.log("4. 빨래질을 한다.")
+console.log("5. 물에 헹군다.")
+ console.log("6. 짠다.")
+ console.log("7. 넌다")
+ }
+
+function 청소(){
+    console.log("1.청소기와 걸레를 준비한다.")
+    console.log("2.청소기를 돌린다.")
+    console.log("3.걸레질을 한다.")
+}
---
+++ day04/out2.js
@@ -0,0 +1,15 @@
+function 비행기예약() {  
+console.log("1. 여행 목적지를 고른다.")
+console.log("2. 날짜를 고른다.")
+console.log("3. 비행사를 고른다.")
+console.log("4. 예약 시간을 고른다.")
+console.log("5. 왕복과 편도를 고른다.")
+console.log("6. 좌석을 고른다.")
+console.log("7. 결제한다.")
+ }
+
+function 쉬다(){
+    console.log("1.침대에 눕는다.")
+    console.log("2.핸드폰을 킨다.")
+    console.log("3.유튜브를 본다.")
+}
